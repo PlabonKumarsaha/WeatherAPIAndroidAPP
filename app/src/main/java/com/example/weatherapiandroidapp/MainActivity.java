5a0a16dc979dd44b747f70b3665551db50e719aa
PKS <pkumarsaha71@gmail.com>
2020-08-15 23:18:04 +0600

    City id by name sucessfull toasted

--- app/src/main/java/com/example/weatherapiandroidapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapiandroidapp/MainActivity.java
@@ -7,6 +7,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +40,53 @@ public class MainActivity extends AppCompatActivity {
         btn_getCityId.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Instantiate the RequestQueue. we are using queue bcz  it will keep the oncomming request in a queue
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://www.metaweather.com/api/location/search/?query="+ET_dataInput.getText().toString();
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        try {
+                            JSONObject cityInfo =response.getJSONObject(0);
+                            String cityId = cityInfo.getString("woeid");
+                            Toast.makeText(getApplicationContext(),"City Id "+cityId,Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(getApplicationContext(),response.toString(),Toast.LENGTH_SHORT).show();
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                // Add the request to the RequestQueue.
+                queue.add(request);
+
+                     // Request a string response from the provided URL.
+               /* StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                              //  textView.setText("Response is: "+ response.substring(0,500));
+                                Toast.makeText(getApplicationContext(),response.toString(),Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                       // textView.setText("That didn't work!");
+                        Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+                    }
+                });*/
+                // Add the request to the RequestQueue.
+              //  queue.add(stringRequest);
 
             }
         });
@@ -41,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         btn_getWeatherByCityName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
